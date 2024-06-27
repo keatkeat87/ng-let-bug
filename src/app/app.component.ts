edf56c5919dad910e3bbe1d97802973f23041bdc
Derrick Yam <keatkeat87@live.com>
2024-06-27 20:58:59 +0800

    ok

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [CommonModule]
+})
+export class AppComponent {}
